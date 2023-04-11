a589798c7fb0b1a2f6a5bfe27c8ce9d6d1b08adc
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-04-11 03:51:24 -0300

    Update networks.constant.ts

--- src/lib/evm-toolkit/constants/networks.constant.ts
+++ src/lib/evm-toolkit/constants/networks.constant.ts
@@ -3,7 +3,7 @@ import type { INetwork } from '../interfaces';
 export const NETWORKS: INetwork[] = [
   {
     "id": 0,
-    "text": "ShimmerEVM Testnet",
+    "text": "BullChain Faucet",
     "apiEndpoint": "https://api.hornet.sc.testnet.shimmer.network",
     "faucetEndpoint": "https://faucet.testnet.shimmer.network",
     "chainAddress": "rms1prwgvvw472spqusqeufvlmp8xdpyxtrnmvt26jnuk6sxdcq2hk8scku26h7",
@@ -13,7 +13,7 @@ export const NETWORKS: INetwork[] = [
   },
   {
     "id": 1,
-    "text": "Custom Network",
+    "text": "BullChainAI",
     "apiEndpoint": "http://localhost:14265",
     "faucetEndpoint": "http://localhost:8091",
     "chainAddress": "tst1pz0kc4urqxf4vh6c556gnr6uwmkwz5ye98vucakgafyneed4gft322s3gf0",
