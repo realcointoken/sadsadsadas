d4f1389a505e93ba6cbd954b79b330637a6918ef
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-04-11 03:56:32 -0300

    Update networks.constant.ts

--- src/lib/evm-toolkit/constants/networks.constant.ts
+++ src/lib/evm-toolkit/constants/networks.constant.ts
@@ -4,12 +4,12 @@ export const NETWORKS: INetwork[] = [
   {
     "id": 0,
     "text": "BullChain Faucet",
-    "apiEndpoint": "https://api.hornet.sc.testnet.shimmer.network",
+    "apiEndpoint": "https://testnet.bullchainai.com/ext/bc/bullchainTestnet/rpc",
     "faucetEndpoint": "https://faucet.testnet.shimmer.network",
-    "chainAddress": "rms1prwgvvw472spqusqeufvlmp8xdpyxtrnmvt26jnuk6sxdcq2hk8scku26h7",
-    "chainID": 1071,
-    "blockExplorer": "https://explorer.evm.testnet.shimmer.network",
-    "networkUrl": "https://json-rpc.evm.testnet.shimmer.network"
+    "chainAddress": "tst1pz0kc4urqxf4vh6c556gnr6uwmkwz5ye98vucakgafyneed4gft322s3gf0",
+    "chainID": 911,
+    "blockExplorer": "https://testnet.bullchainai.com",
+    "networkUrl": "https://testnet.bullchainai.com/ext/bc/bullchainTestnet/rpc"
   },
   {
     "id": 1,
